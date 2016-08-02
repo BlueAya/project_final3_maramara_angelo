2d4849d4241981fd8aa62d922843d629395b7229
Angelo Maramara <angelo.maramara@gmail.com>
2016-08-01 21:47:59 -0400

    Add menu page and footer fix

--- js/main.js
+++ js/main.js
@@ -3,4 +3,10 @@ $(document).ready(function () {
     $('#menu').slicknav();
 
     $('.bxslider').bxSlider();
+
+    $('#footer').css('margin-top',
+  $(document).height()
+  - ( $('#header').height() + $('#content').height() )
+  - $('#footer').height()
+);
 });
