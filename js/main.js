04ea537e77960d8f796178f3ecb760639adeb1eb
Angelo Maramara <angelo.maramara@gmail.com>
2016-08-04 22:01:58 -0400

    Adjust menu icons and create custom JavaScript

--- js/main.js
+++ js/main.js
@@ -14,3 +14,21 @@ $(document).ready(function () {
     );
 
 });
+
+//Custom JavaScript
+//This code listens for when the user clicks on the Toy Story alien image. After clicking on the image, this code adds the text including a promo code for a free game, right above the Awards heading.
+var clickMe = document.getElementById('alien');
+
+var newParagraph = document.createElement('p');
+
+var promoCode = document.createTextNode('Hey buddy! Thanks for showing some intergalactic love. Take this promo code to the game keeper on your next visit for a free try at The Claw: CLAW2016');
+
+var maincontent = document.getElementById('maincontent');
+
+var awards = document.getElementById('awards');
+
+clickMe.addEventListener('click', function () {
+    newParagraph.appendChild(promoCode);
+    newParagraph.setAttribute('style', 'color: #f16b6f');
+    maincontent.insertBefore(newParagraph, awards);
+});
